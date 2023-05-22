042a4644e561cd1c95590d86761a0b8eda7145a6
Sihab Hossain <sihabhossain380@gmail.com>
2023-05-23 00:15:52 +0600

    section title added

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -10,50 +10,57 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper mb-20"
-    >
-      <SwiperSlide>
-        <img src={slide1} alt="" />
-        <h2 className="text-4xl uppercase text-center -mt-20 text-white">
-          Salads
-        </h2>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="" />
-        <h2 className="text-4xl uppercase text-center -mt-20 text-white">
-          Pizzas
-        </h2>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="" />
-        <h2 className="text-4xl uppercase text-center -mt-20 text-white">
-          Soups
-        </h2>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide4} alt="" />
-        <h2 className="text-4xl uppercase text-center -mt-20 text-white">
-          Deserts
-        </h2>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide5} alt="" />
-        <h2 className="text-4xl uppercase text-center -mt-20 text-white">
-          Salads
-        </h2>
-      </SwiperSlide>
-    </Swiper>
+    <section>
+      <SectionTitle
+        subHeading={"From 11.00 am to 10.00 pm"}
+        heading={"Order Online"}
+      ></SectionTitle>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-20"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-20 text-white">
+            Salads
+          </h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-20 text-white">
+            Pizzas
+          </h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-20 text-white">
+            Soups
+          </h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-20 text-white">
+            Deserts
+          </h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-20 text-white">
+            Salads
+          </h2>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
