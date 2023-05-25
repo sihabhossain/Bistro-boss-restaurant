0fcf3cbb4570b25cf3312a35f87d932d2dae4964
Sihab Hossain <sihabhossain380@gmail.com>
2023-05-25 14:24:48 +0600

    login page created

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
         <Link to="/">Home</Link>
         <Link to="/menu">Our Menu</Link>
         <Link to="/order/salad">Order Food</Link>
+        <Link to="/login">Login</Link>
       </li>
     </>
   );
